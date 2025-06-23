15b52fcb6f8ee95aabcad41aef666d6d2b0c80c2
eduardo castro <eduardocastroemp@gmail.com>
2025-06-23 03:21:21 -0300

    feat: upgrade expedients visualization

--- frontend/src/app/(home)/dashboard/profissionais/expedientes/timeCard.tsx
+++ frontend/src/app/(home)/dashboard/profissionais/expedientes/timeCard.tsx
@@ -3,7 +3,7 @@ import { Card } from "@/components/ui/card";
 import { weekdayNames } from "@/constants/weekdayNames";
 import { IExpedient } from "@/types/expedient";
 import { IUser, IUserWithServicesAndExpedients } from "@/types/users";
-import { Clock, Pencil, Trash2 } from "lucide-react";
+import { Calendar, Clock, Pencil, Trash2 } from "lucide-react";
 
 interface ITimeCardProps {
   service: IUserWithServicesAndExpedients["services"][0];
@@ -35,45 +35,69 @@ export default function TimeCard({
     });
   };
 
+  const joinByWeekday = sortExpedients(service.expedients).reduce(
+    (acc: Record<number, IExpedient[]>, expedient) => {
+      if (!acc[expedient.weekday]) {
+        acc[expedient.weekday] = [];
+      }
+
+      acc[expedient.weekday].push(expedient);
+      return acc;
+    },
+    {}
+  );
+
+  console.log(joinByWeekday);
+
   return (
     <div className="grid gap-2 md:grid-cols-2 lg:grid-cols-3">
-      {sortExpedients(service.expedients).map((expedient) => (
-        <Card key={expedient.id} className="p-3 bg-muted/30">
+      {Object.entries(joinByWeekday).map(([weekday, expedients], index) => (
+        <Card key={index} className="p-3 bg-muted/30 h-min">
           <div className="flex items-center justify-between">
-            <div className="space-y-1">
-              <div className="flex items-center gap-1">
-                <Clock className="h-3 w-3 text-muted-foreground" />
+            <div className="flex flex-col w-full space-y-1">
+              <div className="flex items-center gap-2 mb-2">
+                <Calendar className="h-3 w-3 text-muted-foreground" />
                 <span className="text-sm font-medium">
-                  {weekdayNames[expedient.weekday]}
+                  {weekdayNames[Number(weekday)]}
                 </span>
               </div>
-              <div className="text-sm text-muted-foreground">
-                {expedient.startTime} - {expedient.endTime}
-              </div>
-            </div>
-            <div className="flex gap-1">
-              <Button
-                variant="ghost"
-                size="icon"
-                className="h-7 w-7 hover:cursor-pointer hover:bg-yellow-500"
-                onClick={() =>
-                  openEditDialog(expedient, professional.id, service.id)
-                }
-              >
-                <Pencil className="h-3 w-3" />
-                <span className="sr-only">Editar</span>
-              </Button>
-              <Button
-                variant="ghost"
-                size="icon"
-                className="h-7 w-7 hover:cursor-pointer hover:bg-red-500"
-                onClick={() =>
-                  openDeleteDialog(expedient, professional.id, service.id)
-                }
-              >
-                <Trash2 className="h-3 w-3" />
-                <span className="sr-only">Excluir</span>
-              </Button>
+              {expedients.map((expedient) => (
+                <div
+                  key={expedient.id}
+                  className="flex items-center justify-between"
+                >
+                  <div className="flex items-center gap-2">
+                    <Clock className="h-3 w-3 text-muted-foreground" />
+                    <div className="text-sm text-muted-foregroun">
+                      {expedient.startTime} - {expedient.endTime}
+                    </div>
+                  </div>
+                  <div className="flex gap-1">
+                    <Button
+                      variant="ghost"
+                      size="icon"
+                      className="h-7 w-7 hover:cursor-pointer hover:bg-yellow-500"
+                      onClick={() =>
+                        openEditDialog(expedient, professional.id, service.id)
+                      }
+                    >
+                      <Pencil className="h-3 w-3" />
+                      <span className="sr-only">Editar</span>
+                    </Button>
+                    <Button
+                      variant="ghost"
+                      size="icon"
+                      className="h-7 w-7 hover:cursor-pointer hover:bg-red-500"
+                      onClick={() =>
+                        openDeleteDialog(expedient, professional.id, service.id)
+                      }
+                    >
+                      <Trash2 className="h-3 w-3" />
+                      <span className="sr-only">Excluir</span>
+                    </Button>
+                  </div>
+                </div>
+              ))}
             </div>
           </div>
         </Card>
